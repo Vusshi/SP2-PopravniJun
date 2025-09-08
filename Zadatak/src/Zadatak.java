ddac1844b1716847b50b1212175c1a65dbc4fa9d
Vukasin Todorovic <vulet70@gmail.com>
2025-09-08 16:58:05 +0200

    Ispravljeno racunanje sume kvadrata

--- Zadatak/src/Zadatak.java
+++ Zadatak/src/Zadatak.java
@@ -11,8 +11,8 @@ public class Zadatak {
 	
 	public int sumaKvadrata(int n) {
 		int suma = 0;
-		for(int i = 1; i < n; i++) {
-			suma = i*i;
+		for(int i = 0; i < n; i++) {
+			suma = i^2;
 		}
 		return suma;
 	}
